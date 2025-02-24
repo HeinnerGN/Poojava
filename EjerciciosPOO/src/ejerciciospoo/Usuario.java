d2894bb07e2f4eba55f736b06242bbf53d9bdcc5
HeinnerGN <heinnergil@gmail.com>
2025-02-24 09:11:20 -0500

    Add files via upload

---
+++ EjerciciosPOO/src/ejerciciospoo/Usuario.java
@@ -0,0 +1,15 @@
+package ejerciciospoo;
+
+
+public class Usuario {
+    private static String Nombre;
+    private static int Edad;
+    private static int Cedula;
+    
+    public void Usuario(String Nombre, int Edad, int Cedula){
+        Usuario.Nombre=Nombre;
+        Usuario.Edad=Edad;
+        Usuario.Cedula=Cedula;
+    }
+    
+}
