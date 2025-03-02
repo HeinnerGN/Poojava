c7f3d2df093bd37631a0f83560d0e4f1f19f6df9
HeinnerGN <heinnergil@gmail.com>
2025-03-02 18:36:37 -0500

    .

--- Sensor_temperatura/src/sensor_temperatura/Control.java
+++ Sensor_temperatura/src/sensor_temperatura/Control.java
@@ -3,13 +3,13 @@ package sensor_temperatura;
 public class Control {
     public void Controltemp(double temp) {
         if (temp < 10) {
-            System.out.println("Temperatura: " + temp + "C - Encendiendo el calefactor.");
+            System.out.println("Temperatura: " + temp + " C - Encendiendo el calefactor.");
         }
         else if (temp >= 10 && temp <= 25) {
-            System.out.println("Temperatura: " + temp + "C - Sistema inactivo.");
+            System.out.println("Temperatura: " + temp + " C - Sistema inactivo.");
         }
         else {
-            System.out.println("Temperatura: " + temp + "C - Encendiendo el ventilador.");
+            System.out.println("Temperatura: " + temp + " C - Encendiendo el ventilador.");
         }
     }
 }
