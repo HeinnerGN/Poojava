2aef073291a8c72e3d6d23290fc983839ce8e509
HeinnerGN <heinnergil@gmail.com>
2025-03-04 14:51:05 -0500

    .

--- Control_Luces/src/control_luces/Principal.java
+++ Control_Luces/src/control_luces/Principal.java
@@ -3,7 +3,15 @@ package control_luces;
 public class Principal {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        Luces luces = new Luces();
+        Casa casa = new Casa();
+    }
+    
+    public void Detectar(){
+        continuar = false;
+        while (continuar){
+            
+        }
     }
     
 }
