98f82a3b89de9994fc99b68d15c53b67220ffee1
HeinnerGN <heinnergil@gmail.com>
2025-03-04 17:18:14 -0500

    .

--- Control_luces/src/control_luces/Luces.java
+++ Control_luces/src/control_luces/Luces.java
@@ -1,13 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package control_luces;
 
-/**
- *
- * @author heinn
- */
+import java.util.Random;
+
 public class Luces {
+    public static Random dato = new Random();
+    public static String info = ("Luces encendidas.");
+    public static String datazo = ("Luces apagadas.");
+    public static String error = ("Error.");
     
+    public Luces(){
+        boolean continuar = true;
+        while (continuar){
+            boolean denoche = dato.nextBoolean();
+            boolean movi = dato.nextBoolean();
+            
+            if (denoche && movi){
+                System.out.println(info);
+            }
+            else {
+                System.out.println(datazo);
+            }
+            try {
+                Thread.sleep(100);
+            }
+            catch (InterruptedException e){
+                System.out.println(error);
+            }
+        }
+    }
 }
